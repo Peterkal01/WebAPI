6aecc39087a625e8934b3df4904222c51755eedc
Peterkal01 <48078237+Peterkal01@users.noreply.github.com>
2019-10-23 10:33:52 +1100

    api

---
+++ WebAPI/WebAPI/Controllers/EmployeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace WebAPI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "employee" };
+
+           SqlConnection
+        }
